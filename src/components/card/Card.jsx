72b0afbdb1f45ee571e92087ded80abb624391d8
sergiotechx <sergiotechx@yahoo.com>
2023-07-14 19:20:00 -0500

    Con Body

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './card.scss'
+const Card = ({ image, text }) => {
+  return (
+    <div className='CardBody'>
+      <img src={image} />
+      <p>{text}</p>
+    </div>
+  )
+}
+
+export default Card
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
 import "./header.scss"
-import logo from"/public/images/logo.svg"
+import logo from "/public/images/logo.svg"
+import { SlButton, SlDropdown, SlMenu, SlMenuItem } from '@shoelace-style/shoelace/dist/react';
+
 
 const Header = () => {
   return (
     <div className='nav'>
       <nav className='container'>
-        <img className="logo"src={logo}/>
+        <img className="logo" src={logo} />
+        <ul>
+          <li key='1'>About</li>
+          <li key='2'>Careers</li>
+          <li key='3'>Events</li>
+          <li key='4'>Products</li>
+          <li key='5'>Support</li>
+        </ul>
+        <div className='cuadroDeTexto' >
+          <h1>IMMERSIVE EXPERIENCES THAT DELIVER </h1>
+        </div>
+        <SlDropdown>
+          <SlButton  slot="trigger" caret>
+            Options
+          </SlButton>
+          <SlMenu>
+            <SlMenuItem>About</SlMenuItem>
+            <SlMenuItem>Careers</SlMenuItem>
+            <SlMenuItem>Events</SlMenuItem>
+            <SlMenuItem>Products</SlMenuItem>
+            <SlMenuItem>Support</SlMenuItem>
+          </SlMenu>
+        </SlDropdown>
 
-       
-         <ul>
-            <li key='1'>About</li>
-            <li key='2'>Careers</li>
-            <li key='3'>Events</li>
-            <li key='4'>Products</li>
-            <li key='5'>Support</li>
-         </ul>
-         <div className='cuadroDeTexto' >
-            <h1>IMMERSIVE EXPERIENCES THAT DELIVER </h1>
-            </div>         
+
+
       </nav>
+
 
     </div>
   )
