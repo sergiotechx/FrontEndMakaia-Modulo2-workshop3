1447c4b4c016711152437609233ee1a37ca7b05c
Sergio Martinez <64911708+sergiotechx@users.noreply.github.com>
2023-07-18 09:35:13 -0500

    Add files via upload

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,41 @@
+
+import "./header.scss"
+import logo from "/public/images/logo.svg"
+import { SlButton, SlIcon,SlDropdown, SlMenu, SlMenuItem } from '@shoelace-style/shoelace/dist/react';
+
+
+
+const Header = () => {
+  return (
+    <div className='nav'>
+      <nav className='container'>
+        <img className="logo" src={logo} />
+        <ul className='lista'>
+          <li key='1'>About</li>
+          <li key='2'>Careers</li>
+          <li key='3'>Events</li>
+          <li key='4'>Products</li>
+          <li key='5'>Support</li>
+        </ul>
+        <div className='cuadroDeTexto' >
+          <h1>IMMERSIVE EXPERIENCES THAT DELIVER </h1>
+        </div>
+        <SlDropdown className="burguer">
+          <SlButton  slot="trigger" caret>
+            <SlIcon  slot="prefix" name="list"></SlIcon>  
+          </SlButton>
+          
+            <SlMenu>
+              <SlMenuItem>About</SlMenuItem>
+              <SlMenuItem>Careers</SlMenuItem>
+              <SlMenuItem>Events</SlMenuItem>
+              <SlMenuItem>Products</SlMenuItem>
+              <SlMenuItem>Support</SlMenuItem>
+            </SlMenu>
+        </SlDropdown> 
+      </nav>
+    </div>
+  )
+}
+
+export default Header
